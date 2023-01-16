398b33805f16899b094ff4227155a9c10a19cef6
Olintho <olinthojesus@yahoo.com.br>
2023-01-16 17:53:16 -0300

    Creating home page ad=nd email validation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,51 @@
+import { useState } from "react"
+import './home.css'
+
+import { Link } from "react-router-dom"
+
 export default function Home() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    function handleLogin(e) {
+
+        e.preventDefault()
+
+        if(email !==  '' && password !== '') {
+            alert("Teste")
+        } else {
+            alert("Preencha todos os campos!")
+        }
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home-container">
+          <h1>Lista de tarefas</h1>
+          <span>Gerencie sua agenda de forma fácil! </span>
+
+          <form className="form" onSubmit={handleLogin}>
+            <input 
+                autoComplete={false}
+                type="text" 
+                placeholder="Digite seu emelhor e-mail" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input 
+                type="password" 
+                placeholder="********" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Acessar</button>
+
+          </form>
+
+          <Link to="/register" className="button-link">
+            Não possui uma conta? Cadastre-se.
+          </Link>
         </div>
     )
 }
